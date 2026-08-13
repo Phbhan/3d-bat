dce79dbcc1ac8cf16e220ea809bd1a5242b8177f
hanpb2 <v.hanpb2@vinai.io>
2026-08-13 10:01:03 +0700

    refactor: move files preprocess

--- scripts/preprocess/preprocess_data_pipeline.sh
+++ scripts/preprocess/preprocess_data_pipeline.sh
@@ -5,7 +5,7 @@ export INTRINSIC_PATH=/home/hanpb2/workspace/Data/Data_PNK/calib/VF6_03/VF6_03_I
 
 # Merge multiple lidar data into unified dcp file
 mkdir -p $DATA_PROCESSED_PATH/point_clouds/LIDAR_TOP
-python scripts/merge_multi_lidar.py \
+python scripts/preprocess/merge_multi_lidar.py \
     --extr $EXTRINSIC_PATH \
     --lidar_root $DATA_INPUT_PATH/LIDAR \
     --out_dir_pcd $DATA_PROCESSED_PATH/point_clouds/LIDAR_TOP \
@@ -17,7 +17,7 @@ python scripts/merge_multi_lidar.py \
     --visualize_ts 1783757599-099982977
 
 mkdir -p $DATA_PROCESSED_PATH/point_clouds_lidar/LIDAR_TOP
-python scripts/merge_multi_lidar.py \
+python scripts/preprocess/merge_multi_lidar.py \
     --extr $EXTRINSIC_PATH \
     --lidar_root $DATA_INPUT_PATH/LIDAR \
     --out_dir_pcd $DATA_PROCESSED_PATH/point_clouds_lidar/LIDAR_TOP \
@@ -29,20 +29,20 @@ python scripts/merge_multi_lidar.py \
     --visualize_ts 1783757599-099982977
 
 mkdir -p $DATA_PROCESSED_PATH/images_raw
-python scripts/select_camera_data.py \
+python scripts/preprocess/select_camera_data.py \
     --lidar_dir $DATA_PROCESSED_PATH/point_clouds/LIDAR_TOP \
     --camera_root $DATA_INPUT_PATH/CAMERA \
     --out_dir $DATA_PROCESSED_PATH/images_raw
 
 mkdir -p $DATA_PROCESSED_PATH/images_pinhole
-python scripts/undistort_images.py \
+python scripts/preprocess/undistort_images.py \
   --images_root $DATA_PROCESSED_PATH/images_raw \
   --out_root $DATA_PROCESSED_PATH/images_pinhole \
   --intr_path $INTRINSIC_PATH \
   --extr_path $EXTRINSIC_PATH \
   --alpha 0
 
-python scripts/build_calib_json.py \
+python scripts/preprocess/build_calib_json.py \
     --extr_path $EXTRINSIC_PATH \
     --intr_path $DATA_PROCESSED_PATH/images_pinhole/new_intrinsics.json \
     --input_pred_dir "/lustre/scratch/client/vinfast/groups/l4/hanpb2/bevfusion/input_data/20260711_1512_VF6_03_1783757531_1783759331" \
@@ -66,14 +66,14 @@ mv $DATA_PROCESSED_PATH/images_raw/CAM_F_L $DATA_PROCESSED_PATH/images/CAM_FRONT
 mv $DATA_PROCESSED_PATH/images_raw/CAM_F_R $DATA_PROCESSED_PATH/images/CAM_FRONT_RIGHT
 
 # mkdir -p $DATA_PROCESSED_PATH/annotations
-# python scripts/create_empty_annotations.py \
+# python scripts/preprocess/create_empty_annotations.py \
 #     --input_folder_path_point_clouds $DATA_PROCESSED_PATH/point_clouds/LIDAR_TOP \
 #     --output_folder_path_annotations $DATA_PROCESSED_PATH/annotations
 
-python scripts/preprocess_anns.py \
+python scripts/preprocess/preprocess_anns.py \
         --annotations_dir $DATA_PROCESSED_PATH/annotations \
         --rename \
         --modify-box \
         --x-offset -1.403
-python scripts/create_file_name_list.py \
+python scripts/preprocess/create_file_name_list.py \
     --input_folder_path_drive $DATA_PROCESSED_PATH
